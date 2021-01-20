345af8537d8ca48a2301e4c75315c8187e42b8bd
AndreiAkimochkin <AndreiSverige@gmail.com>
2021-01-20 17:24:48 +0100

    15. Select component

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import {UncontrolledAccordion} from "./components/accordion/UncontrolledAccordio
 import {UncontrolledRating} from "./components/rating/UncontrolledRating";
 import {ControlledOnOff} from "./components/OnOff/UncontrolledOnOff";
 import {GetUncontrolledInputValue, UncontrolledInput} from "./components/input/UncontrolledInput";
+import {Select} from "./components/select/Select";
 
 type PageTitlePropsType = {
     title: string
@@ -20,10 +21,30 @@ function App() {
     let [value, setValue]=useState<RatingValueType>(0)
     let [accordionCollapsed, setAccordionCollapsed]=useState<boolean>(false)
     let [on, setOn] = useState(false)
-   
+
+    let [selectValue, setSelectValue] = useState('2')
     return (
         <div>
-            {/*<OnOff/>*/}
+
+            <Select value={selectValue} onChange={setSelectValue} items={[
+                {value: '1', title: 'Stockholm'},
+                {value: '2', title: 'Moskva'},
+                {value: '3', title: 'Kiev'},
+                ]}/>
+
+
+
+
+
+
+
+
+            {/*    <Select value={null} onChange={()=>{}} items={[*/}
+            {/*        {value: '1', title: 'Stockholm'},*/}
+            {/*        {value: '2', title: 'Moskva'},*/}
+            {/*        {value: '3', title: 'Kiev'}*/}
+            {/*    ]}/>*/}
+            {/*/!*<OnOff/>*!/*/}
             {/*<OnOff/>*/}
             {/*<OnOff/>*/}
             {/*<GetUncontrolledInputValue/>*/}
@@ -42,14 +63,14 @@ function App() {
             {/*<PageTitle title="This is APP component"/>*/}
             {/*<PageTitle title="This is not APP component"/>*/}
             {/*Article 1*/}
-            {/*<Rating value={3}/>*/}
-            <Accordion title='First one'
-                       collapsed={accordionCollapsed}
-                       onClick={setAccordionCollapsed}
+            {/*/!*<Rating value={3}/>*!/*/}
+            {/*<Accordion title='First one'*/}
+            {/*           collapsed={accordionCollapsed}*/}
+            {/*           onClick={setAccordionCollapsed}*/}
 
-                        items={[{title:'Andrei', value: 1}, {title:'Bob', value: 2}, {title:'Viktor', value: 3} ]}/>
-            {/*<Accordion title='Second one' collapsed={false}/>*/}
-            {/*Article 2*/}
+            {/*            items={[{title:'Andrei', value: 1}, {title:'Bob', value: 2}, {title:'Viktor', value: 3} ]}/>*/}
+            {/*/!*<Accordion title='Second one' collapsed={false}/>*!/*/}
+            {/*/!*Article 2*!/*/}
             {/*<Rating value={0}/>*/}
             {/*<Rating value={1}/>*/}
             {/*<Rating value={2}/>*/}
--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+import styles from './Select.module.scss'
 
 type ItemType = {
-    value: any
+    value?: any
     title: string
 }
 
 
 type SelectPropsType = {
     value: any
-    onClick: (value:any)=> void
+    onChange: (value:any)=> void
     items: ItemType[]
 }
 
@@ -16,11 +17,37 @@ type SelectPropsType = {
 
 export function Select(props:SelectPropsType) {
 
+    const [active, setActive] = useState(true)
+    const selectedItem =props.items.find(i => i.value === props.value)
+    const toggleItems = ()=>setActive(!active)
+    const onItemClick = (value: any)=> {
+        props.onChange(value)
+        toggleItems()
+    }
+
+
     return (
-        <div>
-            <div>{}</div>
-            <div>{props.items.map((i,index)=>
-                    <li onClick={()=>{props.onClick(i.value)}} key={index}>{i.title}</li>)}</div>
+        <>
+
+            {/*<select>*/}
+            {/*    <option value="">Minsk</option>*/}
+            {/*    <option value="">Moscow</option>*/}
+            {/*    <option value="">Kiev</option>*/}
+            {/*</select>*/}
+            <div className={styles.select }>
+                {/*<div className={styles.select + "" + (active ? styles.active : '')}>*/}
+            <span className={styles.main} onClick={toggleItems}>{selectedItem && selectedItem.title}</span>
+
+           {
+               active &&
+               <div className={styles.items}>
+                {props.items.map(i=> <div className={styles.item}
+                    key={i.value}
+                onClick={()=>(onItemClick(i.value))}
+                >{i.title}</div>)}
+                 </div>
+           }
         </div>
+        </>
     )
 }
